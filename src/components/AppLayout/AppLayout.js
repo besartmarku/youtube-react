68ad8cc91d5f68439aae88c7afd5bbeb0be1b73b
Besart Marku <b.marku@evolutivo.it>
2019-01-29 16:19:15 +0100

    wrap components to AppLayout

---
+++ src/components/AppLayout/AppLayout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./AppLayout.scss";
+import HeaderNav from "../../containers/HeaderNav/HeaderNav";
+
+export function AppLayout(props) {
+  return (
+    <div className="app-layout">
+      <HeaderNav />
+      {props.children}
+    </div>
+  );
+}
+
+export default AppLayout;
