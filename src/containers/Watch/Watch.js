99020ac912b0c4e7f8686c1f22e2d871c6ea4156
Besart Marku <b.marku@evolutivo.it>
2019-01-29 17:33:48 +0100

    Add Video component

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Watch.scss";
+import { Video } from "../../components/Video/Video";
 
 export class Watch extends React.Component {
   render() {
-    return <div>Watch component goes here</div>;
+    return (
+      <div style={{ maxWidth: "80%" }}>
+        <Video id="-7fuHEEmEjs" />
+      </div>
+    );
   }
 }
-
 export default Watch;
