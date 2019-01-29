1d571b5b94824d690663139dbcca0d56ecf49989
Besart Marku <b.marku@evolutivo.it>
2019-01-29 16:36:00 +0100

    Add Routing

--- src/containers/HeaderNav/HeaderNav.js
+++ src/containers/HeaderNav/HeaderNav.js
@@ -2,13 +2,16 @@ import React from "react";
 import { Image, Menu, Form, Input, Icon } from "semantic-ui-react";
 import "./HeaderNav.scss";
 import logo from "../../assets/images/logo.jpg";
+import { Link } from "react-router-dom";
 
 export class HeaderNav extends React.Component {
   render() {
     return (
       <Menu borderless className="top-menu" fixed="top">
         <Menu.Item header className="logo">
-          <Image src={logo} size="tiny" />
+          <Link to="/">
+            <Image src={logo} size="tiny" />
+          </Link>
         </Menu.Item>
         <Menu.Menu className="nav-container">
           <Menu.Item className="search-input">
--- src/containers/HeaderNav/__tests__/HeaderNav.unit.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import HeaderNav from "../HeaderNav";
-
-test("renders HeaderNav", () => {
-  const wrapper = shallow(<HeaderNav />);
-  expect(wrapper).toMatchSnapshot();
-});
