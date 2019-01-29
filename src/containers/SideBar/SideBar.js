823350af067f5d8a3ecf1f6d97cd44d83106a403
Besart Marku <b.marku@evolutivo.it>
2019-01-29 12:12:19 +0100

    polishing sticky side bar

--- src/containers/SideBar/SideBar.js
+++ src/containers/SideBar/SideBar.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { SideBarItem } from "./SideBarItem/SideBarItem";
-import { Menu } from "semantic-ui-react";
+import { SideBarHeader } from "./SideBarHeader/SideBarHeader";
+import Subscriptions from "./Subscriptions/Subscriptions";
+import { SideBarFooter } from "./SideBarFooter/SideBarFooter";
+
+import { Menu, Divider } from "semantic-ui-react";
 import "./SideBar.scss";
 
 class SideBar extends React.Component {
@@ -10,14 +14,19 @@ class SideBar extends React.Component {
         <SideBarItem highlight={true} label="Home" icon="home" />
         <SideBarItem label="Trending" icon="fire" />
         <SideBarItem label="Followers" icon="spy" />
-        {/* <SideBarHeader title="Library" /> */}
+        <SideBarHeader title="Library" />
         <SideBarItem label="History" icon="history" />
+        <Divider />
+        <Subscriptions />
         <SideBarItem label="Watch later" icon="clock" />
         <SideBarItem label="Liked videos" icon="thumbs up" />
         <SideBarItem label="Movies and Shows" icon="film" />
+        <Divider />
         <SideBarItem label="Report history" icon="flag" />
         <SideBarItem label="Help" icon="help circle" />
         <SideBarItem label="Send feedback" icon="comment" />
+        <Divider />
+        <SideBarFooter />
       </Menu>
     );
   }
---
+++ src/containers/SideBar/SideBarFooter/SideBarFooter.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./SideBarFooter.scss";
+
+export function SideBarFooter() {
+  return (
+    <React.Fragment>
+      <div className="footer-block">
+        <div>About Press Copyright</div>
+        <div>Creators Advertise</div>
+        <div>Developers +MyTube</div>
+        <div>Legal</div>
+      </div>
+      <div className="footer-block">
+        <div>Terms Privacy</div>
+        <div>Policy & Safety</div>
+        <div>Test new features</div>
+      </div>
+      <div className="footer-block">
+        <div>All prices include VAT</div>
+      </div>
+      <div className="footer-block">
+        <div>
+          © Productioncoder.com - A Youtube clone for educational purposes under
+          fair use.
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
