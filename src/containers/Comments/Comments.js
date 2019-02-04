0074d1f2dab5a54b8ee8058358cd6948c29feea8
Besart Marku <b.marku@evolutivo.it>
2019-02-04 11:40:03 +0100

    Add Comments

---
+++ src/containers/Comments/Comments.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { CommentsHeader } from "./CommentsHeader/CommentsHeader";
+import { AddComment } from "./AddComment/AddComment";
+import { Comment } from "./Comment/Comment";
+
+export class Comments extends React.Component {
+  render() {
+    return (
+      <div>
+        <CommentsHeader amountComments={this.props.amountComments} />
+        <AddComment />
+        <Comment />
+      </div>
+    );
+  }
+}
+
+export default Comments;
--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -4,6 +4,7 @@ import { RelatedVideos } from "../../components/RelatedVideos/RelatedVideos";
 import { Video } from "../../components/Video/Video";
 import { VideoMetadata } from "../../components/VideoMetadata/VideoMetadata";
 import { VideoInfoBox } from "../../components/VideoInfoBox/VideoInfoBox";
+import { Comments } from "../Comments/Comments";
 
 export class Watch extends React.Component {
   render() {
@@ -12,13 +13,8 @@ export class Watch extends React.Component {
         <Video className="video" id="-7fuHEEmEjs" />
         <VideoMetadata className="metadata" viewCount={1000} />
         <VideoInfoBox className="video-info-box" />
-        <div
-          className="comments"
-          style={{ width: "100%", height: "100px", background: "#9013FE" }}
-        >
-          comments
-        </div>
-        <RelatedVideos className="related-videos" />
+        <Comments className="comments" />
+        <RelatedVideos className="relatedVideos" />
       </div>
     );
   }
