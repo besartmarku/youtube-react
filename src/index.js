75ee772eba3681a046a850dd0ec35fe00ac0f5b2
Besart Marku <b.marku@evolutivo.it>
2019-02-04 12:00:59 +0100

    Adding Redux and redux-saga

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 import "semantic-ui-css/semantic.min.css";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { configureStore } from "./store/configureStore";
+
+const store = configureStore();
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
-
-serviceWorker.unregister();
