54cf5c26a5c11572e3acaa5ebf78a04595da1de6
Besart Marku <besart.marku@gmail.com>
2019-02-14 10:44:22 +0100

    formatting video duration

--- src/components/VideoPreview/VideoPreview.js
+++ src/components/VideoPreview/VideoPreview.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { Image } from "semantic-ui-react";
 import "./VideoPreview.scss";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
+import { getShortNumberString } from "../../services/number/number-format";
+import { getVideoDurationString } from "../../services/date/date-format";
+
+TimeAgo.locale(en);
+const timeAgo = new TimeAgo("en-US");
 
 export class VideoPreview extends React.Component {
   render() {
@@ -8,14 +15,19 @@ export class VideoPreview extends React.Component {
     if (!video) {
       return <div />;
     }
-
+    const duration = video.contentDetails
+      ? video.contentDetails.duration
+      : null;
+    const videoDuration = getVideoDurationString(duration);
+    const viewAndTimeString = VideoPreview.getFormattedViewAndTime(video);
     const horizontal = this.props.horizontal ? "horizontal" : null;
+
     return (
       <div className={["video-preview", horizontal].join(" ")}>
         <div className="image-container">
           <Image src={video.snippet.thumbnails.medium.url} />
           <div className="time-label">
-            <span>{video.contentDetails.duration}</span>
+            <span>{videoDuration}</span>
           </div>
         </div>
 
@@ -26,14 +38,22 @@ export class VideoPreview extends React.Component {
           <div className="video-preview-metadata-container">
             <div className="channel-title">{video.snippet.channelTitle}</div>
             <div>
-              <span>
+              <span>{viewAndTimeString}</span>
+            </div>
+            <div>
+              {/* <span>
                 {video.statistics.viewCount} views • {video.snippet.publishedAt}
-              </span>
+              </span> */}
             </div>
           </div>
         </div>
       </div>
     );
+  }
+  static getFormattedViewAndTime(video) {
+    const publicationDate = new Date(video.snippet.publishedAt);
+    const viewCountShort = getShortNumberString(video.statistics.viewCount);
+    return `${viewCountShort} views • ${timeAgo.format(publicationDate)}`;
   }
 }
 
