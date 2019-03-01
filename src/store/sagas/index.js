fa33e845dcd8e59bcc67e719801b2d172b27b1ea
Besart Marku <b.marku@evolutivo.it>
2019-03-01 12:11:20 +0100

    update sagas to fetch comments

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -5,15 +5,16 @@ import {
   watchVideoCategories
 } from "./video";
 import { watchWatchDetails } from "./watch";
+import { watchCommentThread } from "./comment";
 export default function*() {
   yield all([
     fork(watchMostPopularVideos),
     fork(watchVideoCategories),
     fork(watchMostPopularVideosByCategory),
-    fork(watchWatchDetails)
+    fork(watchWatchDetails),
+    fork(watchCommentThread)
   ]);
 }
-
 export function* fetchEntity(request, entity, ...args) {
   try {
     const response = yield call(request);
