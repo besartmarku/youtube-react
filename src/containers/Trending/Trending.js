616088b76b32a5bdcc4bb9acb0401752148ef79b
Besart Marku <b.marku@evolutivo.it>
2019-04-26 10:59:47 +0200

    Fix Trendig Videos layout

--- src/containers/Trending/Trending.js
+++ src/containers/Trending/Trending.js
@@ -1,57 +1,62 @@
 import React from "react";
-import "./Trending.scss";
-import { VideoPreview } from "../../components/VideoPreview/VideoPreview";
-import { SideBar } from "../SideBar/SideBar";
-
-import * as videoActions from "../../store/actions/video";
-import { getMostPopularVideos } from "../../store/reducers/videos";
-import { getYoutubeLibraryLoaded } from "../../store/reducers/api";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
+import * as videoActions from "../../store/actions/video";
+import {
+  allMostPopularVideosLoaded,
+  getMostPopularVideos,
+  getMostPopularVideosNextPageToken
+} from "../../store/reducers/videos";
+import { getYoutubeLibraryLoaded } from "../../store/reducers/api";
+import { VideoList } from "../../components/VideoList/VideoList";
 
-export class Trending extends React.Component {
-  render() {
-    const previews = this.getVideoPreviews();
-    return (
-      <React.Fragment>
-        <SideBar />
-        <div className="trending">{previews}</div>
-      </React.Fragment>
-    );
-  }
-
-  getVideoPreviews() {
-    return this.props.videos.map(video => (
-      <VideoPreview
-        horizontal={true}
-        expanded={true}
-        video={video}
-        key={video.id}
-        pathname={"/watch"}
-        search={"?v=" + video.id}
-      />
-    ));
-  }
-
+class Trending extends React.Component {
   componentDidMount() {
     this.fetchTrendingVideos();
   }
+
   componentDidUpdate(prevProps) {
     if (prevProps.youtubeLibraryLoaded !== this.props.youtubeLibraryLoaded) {
       this.fetchTrendingVideos();
     }
   }
+
+  render() {
+    const loaderActive = this.shouldShowLoader();
+
+    return (
+      <VideoList
+        bottomReachedCallback={this.fetchMoreVideos}
+        showLoader={loaderActive}
+        videos={this.props.videos}
+      />
+    );
+  }
+
+  fetchMoreVideos = () => {
+    const { nextPageToken } = this.props;
+    if (this.props.youtubeLibraryLoaded && nextPageToken) {
+      this.props.fetchMostPopularVideos(12, true, nextPageToken);
+    }
+  };
+
   fetchTrendingVideos() {
     if (this.props.youtubeLibraryLoaded) {
       this.props.fetchMostPopularVideos(20, true);
     }
+  }
+
+  shouldShowLoader() {
+    return !this.props.allMostPopularVideosLoaded;
   }
 }
 
 function mapStateToProps(state) {
   return {
     videos: getMostPopularVideos(state),
-    youtubeLibraryLoaded: getYoutubeLibraryLoaded(state)
+    youtubeLibraryLoaded: getYoutubeLibraryLoaded(state),
+    allMostPopularVideosLoaded: allMostPopularVideosLoaded(state),
+    nextPageToken: getMostPopularVideosNextPageToken(state)
   };
 }
 
