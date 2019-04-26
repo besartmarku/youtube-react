d6e96cd8c50c65ecf104874f97ed5cc3437e8474
Besart Marku <b.marku@evolutivo.it>
2019-04-26 11:03:47 +0200

    Polishing layout

--- src/containers/SideBar/SideBar.js
+++ src/containers/SideBar/SideBar.js
@@ -1,25 +1,26 @@
 import React from "react";
-import { SideBarItem } from "./SideBarItem/SideBarItem";
-import { SideBarHeader } from "./SideBarHeader/SideBarHeader";
-import Subscriptions from "./Subscriptions/Subscriptions";
-import { SideBarFooter } from "./SideBarFooter/SideBarFooter";
-
+import SideBarItem from "./SideBarItem/SideBarItem";
 import { Menu, Divider } from "semantic-ui-react";
 import "./SideBar.scss";
+import { SideBarHeader } from "./SideBarHeader/SideBarHeader";
+import { Subscriptions } from "./Subscriptions/Subscriptions";
+import { SideBarFooter } from "./SideBarFooter/SideBarFooter";
 
-class SideBar extends React.Component {
+export class SideBar extends React.Component {
   render() {
     return (
       <Menu borderless vertical stackable fixed="left" className="side-nav">
         <SideBarItem path="/" label="Home" icon="home" />
         <SideBarItem path="/feed/trending" label="Trending" icon="fire" />
         <SideBarItem label="Followers" icon="spy" />
+        <Divider />
         <SideBarHeader title="Library" />
         <SideBarItem label="History" icon="history" />
-        <Divider />
-        <Subscriptions />
         <SideBarItem label="Watch later" icon="clock" />
         <SideBarItem label="Liked videos" icon="thumbs up" />
+        <Divider />
+        <Subscriptions />
+        <SideBarHeader title="More from Youtube" />
         <SideBarItem label="Movies and Shows" icon="film" />
         <Divider />
         <SideBarItem label="Report history" icon="flag" />
